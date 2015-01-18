5468a80858f9d9748b3ce75366ac14992ee7709e
Paul Walsh <paulywalsh@gmail.com>
2015-01-18 15:20:57 +0200

    Stub out tests for posting.

---
+++ tests/base.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class BaseTestCase(unittest.TestCase):
+
+    """Common stuff for tests."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
---
+++ tests/post.py
@@ -0,0 +1,16 @@
+from .base import BaseTestCase
+
+
+class TestAPIPost(BaseTestCase):
+
+    def test_post_valid_file_upload(self):
+        self.assertTrue(False)
+
+    def test_post_invalid_file_upload(self):
+        self.assertTrue(False)
+
+    def test_post_valid_url_upload(self):
+        self.assertTrue(False)
+
+    def test_post_invalid_url_upload(self):
+        self.assertTrue(False)
