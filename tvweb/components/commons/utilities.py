61fc743a1f1bf6f57609706cfab3ffd3f2abe3bc
Paul Walsh <paulywalsh@gmail.com>
2015-02-12 13:57:26 +0200

    Allow passing report_limit and row_limit via web interfaces. Closes #12

--- tvweb/components/commons/utilities.py
+++ tvweb/components/commons/utilities.py
@@ -53,6 +53,8 @@ def get_runargs():
     parser.add_argument('schema_url', location=location)
     parser.add_argument('data_file', type=FileStorage, location=location)
     parser.add_argument('schema_file', type=FileStorage, location=location)
+    parser.add_argument('report_limit', type=int, default=1000)
+    parser.add_argument('row_limit', type=int, default=20000)
     return parser.parse_args()
 
 
