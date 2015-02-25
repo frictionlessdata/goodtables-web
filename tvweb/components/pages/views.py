68d4589fc77ab075b289c6e8aca6ec0b20766e65
Paul Walsh <paulywalsh@gmail.com>
2015-02-25 23:31:36 +0200

    Work towards #20.

--- tvweb/components/pages/views.py
+++ tvweb/components/pages/views.py
@@ -45,13 +45,47 @@ class Report(views.MethodView, view_mixins.RunPipelineMixin):
         data = self.get_data(**kwargs)
         if request.args:
             data.update(self.run_pipeline(with_permalinks=True))
+            data.update(self._process_report_data(data['report']))
         return render_template(self.template, **data)
 
     def post(self, **kwargs):
         data = self.get_data(**kwargs)
         if data['form'].validate_on_submit():
             data.update(self.run_pipeline(with_permalinks=True))
+            data.update(self._process_report_data(data['report']))
         return render_template(self.template, **data)
+
+    def _process_report_data(self, report):
+
+        def flatten_results(report):
+            """Flatten results in a report for Web UI."""
+            raw_results = report['results']
+
+            return raw_results
+
+        flat_results = flatten_results(report)
+        flat_result_count = len(flat_results)
+
+        if flat_result_count > 20:
+            result_detail_phrase = 'first {0}'.format(flat_result_count)
+        else:
+            result_detail_phrase = '{0}'.format(flat_result_count)
+
+        processed = {
+            'summary': report['summary'],
+            'columns': report['summary']['columns'],
+            'header_index': report['summary']['header_index'],
+            'row_count': report['summary']['total_row_count'],
+            'column_count': len(report['summary']['columns']),
+            'bad_column_percent': int((report['summary']['bad_column_count'] / len(report['summary']['columns'])) * 100),
+            'bad_row_percent': int((report['summary']['bad_row_count'] / report['summary']['total_row_count']) * 100),
+            'bad_cell_count': 'NaN',
+            'flat_results': flat_results,
+            'flat_result_count': flat_result_count,
+            'result_detail_phrase': result_detail_phrase
+        }
+
+        return processed
 
 
 class Help(views.MethodView):
