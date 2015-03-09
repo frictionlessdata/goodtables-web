dd76547dcf0211e37eda347ed1a3537258b25164
Paul Walsh <paulywalsh@gmail.com>
2015-03-09 11:48:56 +0200

    Add GA script support

--- web/components/commons/context_processors.py
+++ web/components/commons/context_processors.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
 import datetime
 from flask import current_app as app
 
@@ -18,5 +19,6 @@ def inject_app_data():
         'repo': app.config['GOODTABLES_REPO'],
         'new_issue': app.config['GOODTABLES_ISSUES'],
         'datetime': now.strftime('%a %b %d at %I:%M %p'),
-        'year': now.year
+        'year': now.year,
+        'ga_key': os.environ.get('GOOGLE_ANALYTICS_KEY', '')
     }
