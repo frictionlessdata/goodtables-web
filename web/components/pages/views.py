812cd6ef971f600d7409c72bcba5bf3e6bea6f25
Paul Walsh <paulywalsh@gmail.com>
2015-04-22 10:29:11 +0300

    Move docs back into home page

--- web/components/pages/views.py
+++ web/components/pages/views.py
@@ -156,17 +156,3 @@ class Pricing(views.MethodView):
 
     def get(self, **kwargs):
         return render_template(self.template, **self.get_data(**kwargs))
-
-
-class Docs(views.MethodView):
-
-    """Return a Docs page."""
-
-    template = 'pages/docs.html'
-
-    def get_data(self, **kwargs):
-
-        return {}
-
-    def get(self, **kwargs):
-        return render_template(self.template, **self.get_data(**kwargs))
